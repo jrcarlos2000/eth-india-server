4941adb88797c961c1b37dbe540a7c4b89ebc51d
CarlosR <carlos.ramos@grupoimportia.com>
2022-12-03 19:10:46 +0530

    fix  : push integration

--- server.ts
+++ server.ts
@@ -2,20 +2,18 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import morganBody from "morgan-body";
 import bodyParser from "body-parser";
-import {MongoClient, ObjectId} from  "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 // import { compileSolidityCode, findTopMatches ,buildTxPayload, getDiamondFacetsAndFunctions, getDiamondLogs, generateSelectorsData} from "./utils/utils";
 // import { Providers } from "./utils/providers";
 const cors = require("cors");
-const nodeMailer = require('nodemailer');
-const Validator = require('sns-payload-validator');
+const nodeMailer = require("nodemailer");
+const Validator = require("sns-payload-validator");
 
 dotenv.config();
 const corsOptions = {
-  origin: "http://localhost:3000",// TODO : Add custom domain
+  origin: "http://localhost:3000", // TODO : Add custom domain
   optionsSuccessStatus: 200,
 };
-
-
 
 const app: Express = express();
 const port = process.env.PORT || 9000;
@@ -29,125 +27,149 @@ app.use(bodyParser.json());
 // log all requests and responses
 morganBody(app, { logAllReqHeader: true, maxBodyLength: 5000 });
 
-//connect to db 
+//connect to db
 
 let cachedClient = null;
-let cachedDb:any = null;
+let cachedDb: any = null;
 
 const connectToDb = async () => {
+  if (cachedDb) return cachedDb;
 
-  if(cachedDb) return cachedDb;
-
-  const client = await MongoClient.connect(process.env.DATABASE_URL!,{})
+  const client = await MongoClient.connect(process.env.DATABASE_URL!, {});
 
   const db = client.db("hidemelol");
   cachedDb = db;
   cachedClient = client;
 
-  return db
-
-}
+  return db;
+};
 
 let transporter = nodeMailer.createTransport({
-  host: 'mail.privateemail.com',
+  host: "mail.privateemail.com",
   port: 587,
   secureConnection: true,
   auth: {
-      user: process.env.SENDER_EMAIL,
-      pass: process.env.SENDER_EMAIL_PWD
-  }
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.SENDER_EMAIL_PWD,
+  },
 });
 
-transporter.verify((error:any) => {
-  error ? console.log(`There was an error for the email connection: ${error}`) : console.log('Ready to send email')
+transporter.verify((error: any) => {
+  error
+    ? console.log(`There was an error for the email connection: ${error}`)
+    : console.log("Ready to send email");
 });
 
-
-
-app.post("/register-user", async (req : Request, res : Response)=>{
-
+app.post("/register-user", async (req: Request, res: Response) => {
   try {
-    const {address, email} = req.body;
+    const { address, email } = req.body;
     const db = await connectToDb();
-    const exist = await db.collection("users").findOne({address:
-      { $regex: new RegExp("^" + address.toLowerCase(), "i") }});
+    const exist = await db
+      .collection("users")
+      .findOne({
+        address: { $regex: new RegExp("^" + address.toLowerCase(), "i") },
+      });
 
-    if(!exist) db.collection("users").insertOne({address,email});
+    if (!exist) db.collection("users").insertOne({ address, email });
 
     res.status(200).send({
-      msg : "Registered succesfully"
-    })
+      msg: "Registered succesfully",
+    });
   } catch (e) {
     console.log(e);
-    res.status(500).end()
+    res.status(500).end();
   }
-})
+});
 //sns POST
 
-app.post("/sns", async (req : Request , res : Response) => {
+app.post("/sns", async (req: Request, res: Response) => {
   const buffers = [];
 
   for await (const chunk of req) {
-      buffers.push(chunk);
+    buffers.push(chunk);
   }
 
   const data = Buffer.concat(buffers).toString();
 
   if (!data) {
-      console.log("Invalid data received, hence skipping")
-      res.status(200).json({
-          "message": 'Invalid data received'
-      });
-      return;
+    console.log("Invalid data received, hence skipping");
+    res.status(200).json({
+      message: "Invalid data received",
+    });
+    return;
   }
   const payload = JSON.parse(data);
 
   try {
-    await Validator.validate(payload)
-
+    await Validator.validate(payload);
   } catch (err) {
-
-    console.log('payload sender validation failed, hence skipping\n', payload);
+    console.log("payload sender validation failed, hence skipping\n", payload);
     res.status(200).json({
-        "message": 'Your message could not validated'
+      message: "Your message could not validated",
     });
     return;
   }
 
-  res.status(200).send({
-    msg : "Notification sent succesfully"
-  })
+  if (payload.Type === "Notification") {
 
-})
-app.post("/send-create-form-email", async (req : Request, res : Response)=>{
+    const obj = JSON.parse(payload["Message"]);
 
+    if(obj["users"].length > 1) return;
+    if(obj["payload"]["data"]["app"]!="Eth India") return;
+
+    try{
+      const info = await transporter.sendMail({
+        from: process.env.SENDER_EMAIL,
+        to: "daniel2000035@icloud.com",
+        subject: "test",
+        text: "test",
+      });
+
+      console.log(`Message sent: ${info.messageId}`);
+
+    }catch(e){
+      return
+    }
+
+    res.status(200).send({
+      msg: "Message sent successfully",
+    });
+  } else {
+    res.status(500).send({
+      msg: "Message type is wrong",
+    });
+  }
+});
+
+app.post("/send-create-form-email", async (req: Request, res: Response) => {
   try {
-    const {address, payload} = req.body;
+    const { address, payload } = req.body;
     const db = await connectToDb();
-    const item = await db.collection("users").findOne({address:
-      { $regex: new RegExp("^" + address.toLowerCase(), "i") }})
-    
+    const item = await db
+      .collection("users")
+      .findOne({
+        address: { $regex: new RegExp("^" + address.toLowerCase(), "i") },
+      });
+
     const toEmail = item.email;
     let parsedPayload = JSON.parse(payload);
     const info = await transporter.sendMail({
-      from : process.env.SENDER_EMAIL,
-      to : "daniel2000035@icloud.com",
-      subject : "test",
-      text : "test"
-    })
+      from: process.env.SENDER_EMAIL,
+      to: "daniel2000035@icloud.com",
+      subject: "test",
+      text: "test",
+    });
 
-    console.log(`Message sent: ${info.messageId}`)
+    console.log(`Message sent: ${info.messageId}`);
 
     res.status(200).send({
-      msg : "Notification sent succesfully"
-    })
-    
+      msg: "Notification sent succesfully",
+    });
   } catch (e) {
     console.log(e);
-    res.status(500).end()
+    res.status(500).end();
   }
-})
-
+});
 
 // app.post("/add-facet", async (req : Request, res : Response) => {
 
@@ -161,7 +183,7 @@ app.post("/send-create-form-email", async (req : Request, res : Response)=>{
 //     const db = await connectToDb();
 //     const exist = await db.collection("facets").findOne({address:
 //       { $regex: new RegExp("^" + address.toLowerCase(), "i") }})
-  
+
 //     if(!exist){
 //       db.collection("facets").insertOne({name,address,description, abi, timesUsed,audited});
 //       db.collection("selectors").insertMany(selectorsData);
@@ -172,7 +194,7 @@ app.post("/send-create-form-email", async (req : Request, res : Response)=>{
 //     console.error(e);
 //     res.status(500).end();
 //   }
-  
+
 // })
 
 // app.get("/facets", async (req : Request, res: Response) => {
@@ -200,7 +222,7 @@ app.post("/send-create-form-email", async (req : Request, res : Response)=>{
 //     console.log(e);
 //     res.status(500).send();
 //   }
-  
+
 // })
 
 // app.post("/get-facet-selectors", async (req : Request , res : Response) => {
@@ -224,7 +246,7 @@ app.post("/send-create-form-email", async (req : Request, res : Response)=>{
 
 //     const db = await connectToDb()
 //     // const facet = await db.collection("facets").findOne({"_id" : new ObjectId(facetId)})
-//     const facet = await db.collection("facets").findOne({"address" : facetAddr}); 
+//     const facet = await db.collection("facets").findOne({"address" : facetAddr});
 
 //     if(facet && action.toLowerCase() == "add"){
 //       await db.collection("facets").findOneAndUpdate({"address" : facetAddr},{$set:{"timesUsed" : (facet.timesUsed + 1)}}, {new:true});
@@ -241,7 +263,7 @@ app.post("/send-create-form-email", async (req : Request, res : Response)=>{
 //   } catch (e) {
 //     console.log(e);
 //     res.status(500).end();
-//   }  
+//   }
 
 // })
 app.listen(port, () => {
